368bfa34a68809bc5877c13cc574e9bc893e6e7a
Sajad Aliarab <sajad.aliarab@gmail.com>
2024-02-07 23:59:08 +0000

    7th feb 3

--- pages/auth/Login.vue
+++ pages/auth/Login.vue
@@ -1,15 +1,14 @@
 <template>
  <div class="relative h-screen w-full bg-cover bg-gray-800">
-    <form class="max-w-sm mx-auto">
-      <div class="mb-5 pt-20  border-solid border-white">
+    <form class="max-w-sm mx-auto pt-20">
         <UIcon name="i-heroicons-lock-closed"  class=" max-w-sm w-full text-2xl"/>
         <h1 class="w-ful text-center text-2xl font-bold">Login</h1>
+        <div class=" p-2 border rounded-lg bg-gray-900 shadow-3xl">
         <p class=" text-gray-400 p-2">Enter your credentials to access your account.</p>
         <p class=" text-gray-400 p-2">Don't have an account? <NuxtLink to="/auth/register" class="text-primary font-medium">Sign Up</NuxtLink></p>
         <UFormGroup label="Email" required>
         <UInput color="primary" type="email" id="email" class=" w-full p-2.5" placeholder="Enter Your Email Address" />
       </UFormGroup>
-    </div>
       <div class="mb-5">
         <UFormGroup label="Password" required>
         <UInput color="primary" type="password" id="password" class="w-full p-2.5" placeholder="Please Enter your Password"/>
@@ -21,6 +20,7 @@
         </div>
         </div>
       <UButton color="primary" type="submit" class=" font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</UButton>
+    </div>
     </form>
   </div>
     
